13892fd47baa92cd3d0b9ec3bf5c6d0ed5fe59bb
ZerefAintDie <malthafino36@gmail.com>
2025-10-22 12:33:25 +0700

    frontend v1

---
+++ app/login.tsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/navbar";
+import HeroSection from "./components/heroSection";
+
+export default function Login() {
+    return (
+        <>
+            <Navbar />
+            <HeroSection />
+        </>
+    );
+}
